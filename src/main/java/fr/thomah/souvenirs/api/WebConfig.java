b7c700aba1b20ebd5d5a6213291ee1d58875e0e3
Thomah <Thomah 5090230+Thomah@users.noreply.github.com>
2022-10-21 16:48:20 +0200

    fix: set cors origins

--- src/main/java/fr/thomah/souvenirs/api/WebConfig.java
+++ src/main/java/fr/thomah/souvenirs/api/WebConfig.java
@@ -2,6 +2,7 @@ package fr.thomah.souvenirs.api;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -10,11 +11,14 @@ import org.springframework.web.cors.CorsConfiguration;
 @EnableWebSecurity
 public class WebConfig extends WebSecurityConfigurerAdapter {
 
+    @Value("${fr.thomah.souvenirs.api.cors.origins}")
+    private String corsOrigins;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
         corsConfiguration.setAllowedHeaders(List.of("Authorization", "Cache-Control", "Content-Type"));
-        corsConfiguration.setAllowedOrigins(List.of("*"));
+        corsConfiguration.setAllowedOrigins(List.of(corsOrigins.split(",")));
         corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PUT", "OPTIONS", "PATCH", "DELETE"));
         
         // You can customize the following part based on your project, it's only a sample
